c16573103be86175ae86b34a7ab8b54e291ee757
Yash <yashsatyawali70@gmail.com>
2025-03-28 11:36:13 +0530

    Question 6 Solution along with approach added

---
+++ 800 Rated/6-Doremy's-Paint-3.cpp
@@ -0,0 +1,109 @@
+/*
+    we know -> 
+    
+    =>  a1 + a2 = a2 + a3  => a1 = a3  -> It indicates element at 0th , 2nd , 4th ... should be same and 1st , 3rd , 5th ... should be same.
+
+    => Which means only two types of elements must be present and their frequence should be differ by at max 1 Otherwise sum will not become same for all adjacent elements.
+    
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+    int x = 1;
+    while (b)
+    {
+        if (b & 1) x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+void solve(int t){
+    int n;
+    cin >> n;
+    
+    vi nums(n);
+    for(int i = 0 ; i < n ; ++i) cin >> nums[i];
+
+    mii freq;
+    for(int i = 0 ; i < n ; ++i) freq[nums[i]]++;
+
+    if(freq.size() == 1){
+        cout << "YES" << endl;
+    }
+    else if(freq.size() == 2){
+
+        int val1 = freq.begin()->second;
+        int val2 = freq.rbegin()->second;
+    
+        if(abs(val1-val2) <= 1){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    else{
+        cout << "NO" << endl;
+    }
+    
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",  "r",  stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    clock_t z = clock();
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve(t);
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+}
